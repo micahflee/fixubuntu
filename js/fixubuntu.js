36466a6e432c430e96518ea3508c3a4e37b4bee6
Micah Lee <micahflee@riseup.net>
2013-04-12 22:53:37 -0700

    initial commit

---
+++ js/fixubuntu.js
@@ -0,0 +1,57 @@
+$(function(){
+  var fixes = [
+    {
+      name: 'remove amazon ads',
+      cmds: [
+        'sudo apt-get remove -y unity-lens-shopping',
+        'sudo sh -c \'echo "127.0.0.1 productsearch.ubuntu.com" > /etc/hosts\''
+      ]
+    },
+    {
+      name: 'turn off other online search',
+      cmds: ['gsettings set com.canonical.Unity.Lenses remote-content-search none']
+    },
+    {
+      name: 'install gnome',
+      cmds: [
+        'sudo apt-get update',
+        'sudo apt-get install -y gnome-shell',
+        'sudo /usr/lib/lightdm/lightdm-set-defaults -s gnome'
+      ]
+    },
+    {
+      name: 'logout of unity',
+      cmds: ['sudo killall gnome-session']
+    }
+  ];
+  
+  // draw fix buttons
+  $.each(fixes, function(){
+    var $button = $('<div class="fix-button">'+this.name+'</div>')
+      .data('cmds', this.cmds)
+      .click(function(){
+        if($(this).hasClass('off')) {
+          $(this).removeClass('off');
+          update_code();
+        } else {
+          $(this).addClass('off');
+          update_code();
+        }
+      });
+    $('#fixes').append($button);
+  });
+  
+  // update code
+  function update_code(){
+    var code = '';
+    $('.fix-button').each(function(){
+      if(!$(this).hasClass('off')) {
+        $.each($(this).data('cmds'), function(){
+          code += this + '; ';
+        });
+      }
+    });
+    $('#cmd').html(code);
+  }
+  update_code();
+});
