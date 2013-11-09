596a8aa0d145db1a2dc5aa70ecb1fc47190ba3ce
Micah Lee <micah@micahflee.com>
2013-11-09 14:38:25 -0800

    updated script to address suggestions in #2, #6, #9

--- fixubuntu.sh
+++ fixubuntu.sh
@@ -6,11 +6,21 @@
 
 gsettings set com.canonical.Unity.Lenses remote-content-search none; 
 
-if [ "`/usr/bin/lsb_release -rs`" \< 13.10 ]; then
-  sudo apt-get remove -y unity-lens-shopping; 
-else 
-  gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"; 
-fi; 
+V=`/usr/bin/lsb_release -rs`
 
-echo | sudo tee -a /etc/hosts
-echo 127.0.0.1 productsearch.ubuntu.com | sudo tee -a /etc/hosts
+if [ $V \< 12.10 ]; then
+  echo "Good news! Your version of Ubuntu doesn't invade your privacy."
+else
+  if [ $V \< 13.10 ]; then
+    sudo apt-get remove -y unity-lens-shopping; 
+  else 
+    gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"; 
+  fi
+
+  if ! grep -q productsearch.ubuntu.com /etc/hosts; then 
+    echo -e "\n127.0.0.1 productsearch.ubuntu.com" | sudo tee -a /etc/hosts >/dev/null
+  fi
+
+  echo "All done. Enjoy your privacy."
+fi
+
